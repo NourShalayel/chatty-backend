ebdbf8a0d25700cf3979549aca2404af2ca7e1d6
Nour Shalayel <noorshalayel@gmail.com>
2024-08-07 22:28:11 +0300

    update routes

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { config } from "./config";
+import { config } from "@root/config";
 import Logger from "bunyan";
 
 const log: Logger = config.createLogger("setupDatabase");
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -17,13 +17,10 @@ import { createClient } from "redis";
 import { Server } from "socket.io";
 import { createAdapter } from "@socket.io/redis-adapter";
 import "express-async-errors";
-import { config } from "./config";
-import applicationRoutes from "./routes";
-import {
-  CustomError,
-  IErrorResponse,
-} from "./shared/globals/helpers/error-handler";
+import { config } from "@root/config";
+import applicationRoutes from "@root/routes";
 import Logger from "bunyan";
+import { CustomError, IErrorResponse } from "@global/helpers/error-handler";
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger("server");
