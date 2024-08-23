090fcdee31cf31c58748a6434e255b4d8ef8fbfe
Nour Shalayel <noorshalayel@gmail.com>
2024-08-23 22:35:47 +0300

    get current user

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { authRoutes } from "@auth/routes/authRoutes";
+import { currentUserRoutes } from "@auth/routes/currentRoutes";
+import { authMiddlerWare } from "@global/helpers/auth-middlerware";
 import { serverAdapter } from "@service/queues/base.queue";
 import { Application } from "express";
 
@@ -6,9 +8,11 @@ import { Application } from "express";
 const BASE_PATH = '/api/v1'
 export default (app: Application) => {
   const routes = () => {
-    app.use('/queues' , serverAdapter.getRouter());
+    app.use('/queues', serverAdapter.getRouter());
     app.use(BASE_PATH, authRoutes.routes());
     app.use(BASE_PATH, authRoutes.signOutRoute());
+
+    app.use(BASE_PATH, authMiddlerWare.verfiyUser, currentUserRoutes.routes());
 
   };
 
