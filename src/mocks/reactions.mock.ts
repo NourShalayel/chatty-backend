1e80e47f7ec6a7604e528c0683330d6903304115
Nour Shalayel <noorshalayel@gmail.com>
2024-09-12 00:49:42 +0300

    add features comments

--- src/mocks/reactions.mock.ts
+++ src/mocks/reactions.mock.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import { AuthPayload } from '@auth/interfaces/auth.interface';
 import { IReactionDocument, IReactions } from '@reaction/interfaces/reaction.interface';
 import { IJWT } from './auth.mock';
+import { ICommentDocument, ICommentNameList } from '@comment/interfaces/comment.interface';
 
 export const reactionMockRequest = (sessionData: IJWT, body: IBody, currentUser?: AuthPayload | null, params?: IParams) => ({
   session: sessionData,
@@ -39,7 +40,7 @@ export interface IParams {
 
 export const reactionData: IReactionDocument = {
   _id: '6064861bc25eaa5a5d2f9bf4',
-  username: 'Danny',
+  username: 'Nour',
   postId: '6027f77087c9d9ccb1555268',
   profilePicture: 'https://res.cloudinary.com/ratingapp/image/upload/6064793b091bf02b6a71067a',
   comment: 'This is a comment',
@@ -48,3 +49,18 @@ export const reactionData: IReactionDocument = {
   type: 'love'
 } as IReactionDocument;
 
+export const commentsData: ICommentDocument = {
+  _id: '6064861bc25eaa5a5d2f9bf4',
+  username: 'Nour',
+  avatarColor: '#9c27b0',
+  postId: '6027f77087c9d9ccb1555268',
+  profilePicture: '',
+  comment: 'This is a comment',
+  createdAt: new Date(),
+  userTo: '60263f14648fed5246e322d9'
+} as unknown as ICommentDocument;
+
+export const commentNames: ICommentNameList = {
+  count: 1,
+  names: ['Nour']
+};
